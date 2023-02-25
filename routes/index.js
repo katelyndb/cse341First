cdaef230d906ed45d47fd8f6c85d3120ae471f3b
Katie Blunck <97479562+katelyndb@users.noreply.github.com>
2023-02-25 10:32:24 -0700

    Testing Stuff

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 // responds with the home page route (when get request is made)
 router.get('/', (req, res) => {
     res.send('Nia Goodwill')
-  })
+  });
   // the about route
   router.get('/about', (req, res) => {
     res.send('About Nia Goodwill')
-  })
+  });
+
+router.get('/api/course/:id', (req,res) => {
+  res.send(req.params.id);
+});
 
 
-  module.exports = router
+  module.exports = router;
