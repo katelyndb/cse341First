250b280a4f5b28f9c804968a2cd1a637893bed6a
Katie Blunck <97479562+katelyndb@users.noreply.github.com>
2023-02-24 22:35:15 -0700

    Working on L02 Assignments

---
+++ demo.js
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv");
+dotenv.config()
+
+
+const { MongoClient } = require ('mongodb');
+
+async function main() {
+   
+    const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.njj0t01.mongodb.net/?retryWrites=true&w=majority`;
+
+    const client = new MongoClient(uri);
+
+    try{
+        await client.connect();
+        await listDatabases(client);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+    
+}
+
+main().catch(console.error);
+
+async function createListing(client, newListing) {
+   const result = await client.db("NAMEOFDATABASE").collection("NAMEOFCOLLECTION").insertOne(newListing);
+   console.log(`New Listing created with the following id: ${result.insertedId}`);
+}
+
+
+async function listDatabases(client) {
+    const databasesList = await client.db().admin().listDatabases();
+    console.log("Databases");
+    databasesList.databases.forEach(db =>{
+        console.log(`- ${db.name}`);
+    })
+}
