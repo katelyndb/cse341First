57347808631846b8101d85523b46fa61611a94c3
Katie Blunck <97479562+katelyndb@users.noreply.github.com>
2023-03-01 08:21:33 -0700

    Unsure

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
-
+const bodyParser = require("body-parser");
+const mongodb = require("./db/connect");
 
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     next();
   })
-  .use('/', require('./routes'));
+  .use("/", require("./routes"));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
