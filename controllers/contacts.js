cb11cfe373b41b20fe8d0d47d3b16f388e9385a4
Katie Blunck <97479562+katelyndb@users.noreply.github.com>
2023-03-01 10:43:48 -0700

    Got the delete contact working!

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAllData = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
+  const result = await mongodb.getDb('cse341first').db('cse341first').collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -13,7 +13,7 @@ const getAllData = async (req, res, next) => {
 const getSingleData = async (req, res, next) => {
   // gets parameter
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
+  const result = await mongodb.getDb('cse341first').db('cse341first').collection('contacts').find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
@@ -21,8 +21,9 @@ const getSingleData = async (req, res, next) => {
 
 }
 
-const createContact = async (req, res, next, newContact) => {
-  const result = await mongodb.getDb().db().collection('contacts').insertOne(newContact);
+const createContact = async (req, res, next) => {
+  const contactData = new ObjectId(req.params.contactData);
+  const result = await mongodb.getDb('cse341first').db('cse341first').collection('contacts').insertOne(contactData);
   console.log(`New Contact created with the following Id: ${result.insertedId}`);
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
@@ -30,8 +31,9 @@ const createContact = async (req, res, next, newContact) => {
   });
 }
 
-const createMultipleContacts = async (req, res, next, newContacts) => {
-  const result = await mongodb.getDb().db().collection('contacts').insertMany(newContacts);
+const createMultipleContacts = async (req, res, next ) => {
+  const contactsData = new ObjectId(req.params.contactsData);
+  const result = await mongodb.getDb('cse341first').db('cse341first').collection('contacts').insertMany(contactsData);
   console.log(` ${result.insertedCount} new contacts were created with the following ids: `);
   console.log(result.insertedIds)
   result.toArray().then((lists) => {
@@ -43,7 +45,7 @@ const createMultipleContacts = async (req, res, next, newContacts) => {
 const updateContactById = async(req, res, next ) => {
   const userId = new ObjectId(req.params.id);
   const updatedContact = new ObjectId(req.params.updatedContact)
-  const result = await mongodb.getDb().db().collection('contacts').updateOne({ _id: userId },{ $set: updatedContact });
+  const result = await mongodb.getDb('cse341first').db('cse341first').collection('contacts').updateOne({ _id: userId },{ $set: updatedContact });
   console.log(`${result.matchedCount} document(s) matched the query criteria`);
   console.log(`${result.modifiedCount} was/were updated`);
   result.toArray().then((lists) => {
@@ -55,13 +57,15 @@ const updateContactById = async(req, res, next ) => {
 
 const deleteContactById = async(req, res, next ) => {
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId });
-  console.log(`${result.matchedCount} document(s) matched the query criteria`);
-  console.log(`${result.modifiedCount} was/were deleted`);
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  const result = await mongodb.getDb('cse341first').db('cse341first').collection('contacts').deleteOne({ _id: userId });
+  console.log(result);
+  if (result.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Error occured while trying to delete the contact.');
+  }
+  
+ 
 }
 
 module.exports = { getAllData, getSingleData, createContact, createMultipleContacts, updateContactById, deleteContactById };
--- routes/contacts.js
+++ routes/contacts.js
@@ -6,5 +6,13 @@ const contactsController = require('../controllers/contacts');
 router.get('/', contactsController.getAllData);
 // Gets a specific contact
 router.get('/:id', contactsController.getSingleData);
+// Creates a new contact
+router.get('/newContact/:contactData', contactsController.createContact);
+// Creates multiple new Contacts
+router.get('/newContacts/:contactsData', contactsController.createMultipleContacts);
+// Updates the contact by the ID
+router.get('/updateContact/:id/:updatedContact', contactsController.updateContactById);
+// Deletes the contact by the ID
+router.get('/deletecontact/:id', contactsController.deleteContactById);
 
 module.exports = router;
